917b5687ba72f853d7df09a2603f44ceebcb9c4c
Felipe Leal <felipewanderson2020@gmail.com>
2020-10-16 19:04:56 -0300

    :construction: Aula 02

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello World'});
-})
+
 
 app.listen(3333, () => {
   console.log('Server running...');
